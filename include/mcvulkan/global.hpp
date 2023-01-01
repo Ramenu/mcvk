88ac61e00cb8c5df17c3bd1efbd4df233a21619e
ramenu <ramenu101@gmail.com>
2023-01-01 18:51:26 -0500

    add compare enum

--- include/mcvulkan/global.hpp
+++ include/mcvulkan/global.hpp
@@ -3,6 +3,20 @@
 
 namespace Global
 {
+    // Should be used for comparsions between two values/objects.
+    // Order should be:
+    // $1 > $2: Greater
+    // $1 = $2: Equal
+    // $1 < $2: Less
+    // If a comparsion fails for whatever reason, use Fail
+    enum class Compare
+    {
+        Greater,
+        Equal,
+        Less,
+        Fail
+    };
+
     #ifndef NDEBUG
         static constexpr bool IS_DEBUG_BUILD = true;
     #else
