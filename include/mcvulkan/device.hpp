fa765fc7d56f1f84a7319d0ddac0186221eb37dc
ramenu <ramenu101@gmail.com>
2023-01-02 18:05:37 -0500

    add LogicalDevice class

--- include/mcvulkan/device.hpp
+++ include/mcvulkan/device.hpp
@@ -2,10 +2,52 @@
 #define MCVULKAN_DEVICE_HPP
 
 #include <vulkan/vulkan.h>
+#include "mcvulkan/queue.hpp"
 
 namespace Device
 {
-    extern void select_physical_device(VkInstance instance) noexcept;
+    struct DeviceInfo
+    {
+        VkPhysicalDevice device {};
+        VkPhysicalDeviceProperties properties {};
+        VkPhysicalDeviceFeatures features {};
+        VkMemoryHeap memory_heap {};
+        Queue::QueueFamilyIndices queue_family_indices {};
+    };
+
+    class LogicalDevice
+    {
+        private:
+            VkDevice device {VK_NULL_HANDLE};
+            VkQueue graphics_queue {};
+        public:
+            //LogicalDevice(const DeviceInfo &selected_device_info) noexcept; 
+            constexpr LogicalDevice() noexcept = default;
+            explicit LogicalDevice(const DeviceInfo &selected_device_info) noexcept;
+            constexpr LogicalDevice& operator=(LogicalDevice &&other) noexcept
+            {
+                this->device = other.device;
+                this->graphics_queue = other.graphics_queue;
+
+                other.device = VK_NULL_HANDLE;
+                return *this;
+            }
+            constexpr explicit LogicalDevice(LogicalDevice &&other) noexcept
+            {
+                this->device = other.device;
+                this->graphics_queue = other.graphics_queue;
+
+                other.device = VK_NULL_HANDLE;
+            }
+            // delete copy operations
+            LogicalDevice(const LogicalDevice &other) noexcept = delete;
+            LogicalDevice& operator=(const LogicalDevice &other) noexcept = delete;
+
+            ~LogicalDevice() noexcept; 
+            constexpr auto get() const { return device; }
+    };
+
+    extern DeviceInfo select_physical_device(VkInstance instance) noexcept;
 }
 
 #endif // MCVULKAN_DEVICE_HPP
