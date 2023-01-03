6787c57af0840b3b50fa46a8c5324f35f7d0f98a
ramenu <ramenu101@gmail.com>
2023-01-02 19:29:37 -0500

    delete move/copy ctors/assignments

--- include/mcvulkan/window.hpp
+++ include/mcvulkan/window.hpp
@@ -2,6 +2,7 @@
 #define MCVULKAN_WINDOW_HPP
 
 #include <GLFW/glfw3.h>
+#include "global.hpp"
 
 class Window
 {
@@ -10,6 +11,9 @@ class Window
         GLFWwindow *window {nullptr};
     public:
         Window(unsigned width, unsigned height, const char *name) noexcept;
+        Window(const Window &window) = delete;
+        Window &operator=(const Window &window) = delete;
+        NON_MOVABLE_DEFAULT(Window) // Delete default move assignment/constructor
         ~Window() noexcept;
         constexpr const auto get() const { return window; }
 };
