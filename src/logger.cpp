894523e81d086c6917a19f8daf8799a8038c2a2d
ramenu <ramenu101@gmail.com>
2023-01-04 19:19:02 -0500

    add docs for logger, as well as mutex

--- src/logger.cpp
+++ src/logger.cpp
@@ -1,12 +1,20 @@
 #include <cstdio>
 #include <cstdlib>
+#include <mutex>
 #include "mcvulkan/color.h"
 
 
 namespace Logger
 {
-    void fatal_error(const char *msg) noexcept
+
+    static std::mutex mtx;
+
+    [[noreturn]] void fatal_error(const char *msg) noexcept
     {
+        // Need a lock here because exit is not thread-safe, it uses a
+        // global variable which is not protected so a race condition can
+        // occur. 
+        std::lock_guard lock {mtx};
         fprintf(stderr, COLOR_BOLDRED "FATAL ERROR: " COLOR_RESET "%s\n", msg);
         exit(EXIT_FAILURE);
     }
@@ -16,18 +24,20 @@ namespace Logger
         fprintf(stderr, COLOR_RED "ERROR: " COLOR_RESET "%s\n", msg);
     }
 
-    void diagnostic(const char *msg) noexcept
-    {
-        fprintf(stderr, COLOR_MAGENTA "DIAGNOSTIC: " COLOR_RESET "%s\n", msg);
-    }
+    #ifndef NDEBUG
+        void diagnostic(const char *msg) noexcept
+        {
+            fprintf(stdout, COLOR_MAGENTA "DIAGNOSTIC: " COLOR_RESET "%s\n", msg);
+        }
 
-    void info(const char *msg) noexcept
-    {
-        fprintf(stderr, COLOR_MAGENTA "INFO: " COLOR_RESET "%s\n", msg);
-    }
+        void info(const char *msg) noexcept
+        {
+            fprintf(stdout, COLOR_MAGENTA "INFO: " COLOR_RESET "%s\n", msg);
+        }
 
-    void warning(const char *msg) noexcept
-    {
-        fprintf(stderr, COLOR_YELLOW "WARNING: " COLOR_RESET "%s\n", msg);
-    }
+        void warning(const char *msg) noexcept
+        {
+            fprintf(stderr, COLOR_YELLOW "WARNING: " COLOR_RESET "%s\n", msg);
+        }
+    #endif
 }
