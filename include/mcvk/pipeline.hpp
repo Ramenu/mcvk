cbf682102935f1ce34f853da180c2588e897c99c
ramenu <ramenu101@gmail.com>
2023-01-28 12:47:30 -0500

    feat: get graphics pipeline working

--- include/mcvk/pipeline.hpp
+++ include/mcvk/pipeline.hpp
@@ -1,27 +1,38 @@
 #ifndef MCVK_PIPELINE_HPP
 #define MCVK_PIPELINE_HPP
 
-#include <vulkan/vulkan.h>
-#include "global.hpp"
-#include "logger.hpp"
-#include <cassert>
+#include <vulkan/vulkan_core.h>  // for VK_NULL_HANDLE, vkDestroyPipelineLayout
+#include <cassert>               // for assert
+#include "global.hpp"            // for DELETE_NON_COPYABLE_NON_MOVABLE_DEFAULT
+#include "logger.hpp"            // for info
+#include "shader.hpp"            // for Shader
 
 class Pipeline
 {
     private:
         VkPipelineLayout pipeline_layout {VK_NULL_HANDLE};
+        VkPipeline pipeline {VK_NULL_HANDLE};
+        VkRenderPass renderpass {VK_NULL_HANDLE};
+        Shader shader {};
         const VkDevice device_handle {VK_NULL_HANDLE};
+        void init_renderpass(VkFormat swapchain_image_format) noexcept;
     public:
-        Pipeline(VkDevice device_handle) noexcept;
+        explicit Pipeline(VkDevice device_handle, VkFormat swapchain_image_format) noexcept;
         DELETE_NON_COPYABLE_NON_MOVABLE_DEFAULT(Pipeline)
         ~Pipeline() noexcept
         {
             if constexpr (Global::IS_DEBUG_BUILD) {
                 assert(device_handle != VK_NULL_HANDLE);
                 assert(pipeline_layout != VK_NULL_HANDLE);
+                assert(pipeline != VK_NULL_HANDLE);
+                assert(renderpass != VK_NULL_HANDLE);
+                Logger::info("De-allocating 'VkRenderPass'");
                 Logger::info("De-allocating 'VkPipelineLayout'");
+                Logger::info("De-allocating 'VkPipeline'");
             }
+            vkDestroyPipeline(device_handle, pipeline, nullptr);
             vkDestroyPipelineLayout(device_handle, pipeline_layout, nullptr);
+            vkDestroyRenderPass(device_handle, renderpass, nullptr);
         }
 
 };
--- src/pipeline.cpp
+++ src/pipeline.cpp
@@ -1,18 +1,27 @@
 #include "mcvk/pipeline.hpp"
-#include <array>
 
-// these warning pushes are temporary
-#pragma GCC diagnostic push
-#pragma GCC diagnostic ignored "-Wunused-but-set-variable"
+#include <array>            // for array
+#include <fstream> // IWYU pragma: keep
+#include <string> // IWYU pragma: keep
+#include <sstream> // IWYU pragma: keep
+
+#include "mcvk/global.hpp"  // for IS_DEBUG_BUILD
+#include "mcvk/logger.hpp"  // for fatal_error
+#include "mcvk/types.hpp"
+
 
 static constexpr std::array DYNAMIC_STATES {
     VK_DYNAMIC_STATE_VIEWPORT,
-    VK_DYNAMIC_STATE_LINE_WIDTH
+    VK_DYNAMIC_STATE_SCISSOR
 };
 
-Pipeline::Pipeline(VkDevice device) noexcept :
+static VkShaderModule create_shader_module(const std::string &code, VkDevice device) noexcept;
+
+Pipeline::Pipeline(VkDevice device, const VkFormat swapchain_image_format) noexcept :
     device_handle {device}
 {
+    if constexpr (Global::IS_DEBUG_BUILD)
+        assert(device_handle != VK_NULL_HANDLE);
     VkPipelineDynamicStateCreateInfo dynamic_state_info {};
     dynamic_state_info.sType = VK_STRUCTURE_TYPE_PIPELINE_DYNAMIC_STATE_CREATE_INFO;
     dynamic_state_info.dynamicStateCount = DYNAMIC_STATES.size();
@@ -32,7 +41,12 @@ Pipeline::Pipeline(VkDevice device) noexcept :
     input_assembly_info.topology = VK_PRIMITIVE_TOPOLOGY_TRIANGLE_LIST;
     input_assembly_info.primitiveRestartEnable = VK_FALSE;
 
-    // viewport and scissoring are done dynamically (at drawing time) so no need to specify them here
+    // viewport and scissoring are done dynamically (at drawing time) so we don't need to really
+    // setup anything here, asides from just creating the struct and setting the viewport and scissor count
+    VkPipelineViewportStateCreateInfo viewport_info {};
+    viewport_info.sType = VK_STRUCTURE_TYPE_PIPELINE_VIEWPORT_STATE_CREATE_INFO;
+    viewport_info.viewportCount = 1;
+    viewport_info.scissorCount = 1;
 
     // the rasterizer takes the geometry shaped by the vertices from 
     // the vertex shader and turns it into fragments to be colored by the 
@@ -97,6 +111,170 @@ Pipeline::Pipeline(VkDevice device) noexcept :
     color_blending_info.blendConstants[2] = 0.0f; // optional
     color_blending_info.blendConstants[3] = 0.0f; // optional
 
+    VkPipelineLayoutCreateInfo pipeline_layout_info {};
+    pipeline_layout_info.sType = VK_STRUCTURE_TYPE_PIPELINE_LAYOUT_CREATE_INFO;
+    pipeline_layout_info.setLayoutCount = 0; // optional
+    pipeline_layout_info.pSetLayouts = nullptr; // optional
+    pipeline_layout_info.pushConstantRangeCount = 0; // optional
+    pipeline_layout_info.pPushConstantRanges = nullptr; // optional
+
+    if (vkCreatePipelineLayout(device_handle, &pipeline_layout_info, nullptr, &pipeline_layout) != VK_SUCCESS)
+        Logger::fatal_error("Failed to create pipeline layout");    // open files
+
+    this->init_renderpass(swapchain_image_format);
+
+    /**
+     * Move this later out of the pipeline constructor later.
+     * Should belong in a shader class?
+     */
+    const char *vertex_shader_path = "./shaders/vertex.spv";
+    const char *fragment_shader_path = "./shaders/fragment.spv";
+
+    std::ifstream vertex_shader_file {vertex_shader_path, std::ios::binary};
+    std::ifstream fragment_shader_file {fragment_shader_path, std::ios::binary};
+
+    if (!vertex_shader_file.is_open())
+        Logger::fatal_error(std::string{std::string{"Failed to open vertex shader file: "} + vertex_shader_path}.c_str());
+    if (!fragment_shader_file.is_open())
+        Logger::fatal_error(std::string{std::string{"Failed to open fragment shader file: "} + fragment_shader_path}.c_str());
+
+    // read contents from files
+    std::stringstream vertex_shader_stream;
+    std::stringstream fragment_shader_stream;
+
+    vertex_shader_stream << vertex_shader_file.rdbuf();
+    fragment_shader_stream << fragment_shader_file.rdbuf();
+
+    vertex_shader_file.close();
+    fragment_shader_file.close();
+
+    const auto vertex_shader_code = vertex_shader_stream.str();
+    const auto fragment_shader_code = fragment_shader_stream.str();
+
+    // create shader modules and setup stages
+    const auto vertex_shader_module = create_shader_module(vertex_shader_code, device);
+    const auto fragment_shader_module = create_shader_module(fragment_shader_code, device);
+
+    VkPipelineShaderStageCreateInfo vertex_shader_stage_info {}, fragment_shader_state_info {};
+    vertex_shader_stage_info.sType = VK_STRUCTURE_TYPE_PIPELINE_SHADER_STAGE_CREATE_INFO;
+    vertex_shader_stage_info.stage = VK_SHADER_STAGE_VERTEX_BIT;
+    vertex_shader_stage_info.module = vertex_shader_module;
+    vertex_shader_stage_info.pName = "main";
+
+    fragment_shader_state_info.sType = VK_STRUCTURE_TYPE_PIPELINE_SHADER_STAGE_CREATE_INFO;
+    fragment_shader_state_info.stage = VK_SHADER_STAGE_FRAGMENT_BIT;
+    fragment_shader_state_info.module = fragment_shader_module;
+    fragment_shader_state_info.pName = "main";
+
+    const std::array shader_stages {vertex_shader_stage_info, fragment_shader_state_info};
+
+    // now we can finally create the pipeline
+    VkGraphicsPipelineCreateInfo pipeline_info {};
+    pipeline_info.sType = VK_STRUCTURE_TYPE_GRAPHICS_PIPELINE_CREATE_INFO;
+    pipeline_info.stageCount = shader_stages.size();
+    pipeline_info.pStages = shader_stages.data();
+    pipeline_info.pVertexInputState = &vertex_input_info;
+    pipeline_info.pInputAssemblyState = &input_assembly_info;
+    pipeline_info.pViewportState = &viewport_info;
+    pipeline_info.pRasterizationState = &rasterizer_info;
+    pipeline_info.pMultisampleState = &multisampling_info;
+    pipeline_info.pDepthStencilState = nullptr; // optional
+    pipeline_info.pColorBlendState = &color_blending_info;
+    pipeline_info.pDynamicState = &dynamic_state_info; 
+
+    pipeline_info.layout = this->pipeline_layout;
+    pipeline_info.renderPass = this->renderpass;
+    pipeline_info.subpass = 0;
+
+    // Vulkan allows you to create a new pipeline by deriving from an existing one. This allows for
+    // better performance. Right now there is only a single pipeline so we don't need it. You can also
+    // specify the index of the pipeline to derive from, which is what 'basePipelineIndex' is for.
+    pipeline_info.basePipelineHandle = VK_NULL_HANDLE; // optional
+    pipeline_info.basePipelineIndex = -1; // optional
+
+    if (vkCreateGraphicsPipelines(device_handle, VK_NULL_HANDLE, 1, &pipeline_info, nullptr, &pipeline) != VK_SUCCESS)
+        Logger::fatal_error("Failed to create graphics pipeline");
+
+    vkDestroyShaderModule(device, vertex_shader_module, nullptr);
+    vkDestroyShaderModule(device, fragment_shader_module, nullptr);
 
 }
-#pragma GCC diagnostic pop
+
+static VkShaderModule create_shader_module(const std::string &code, VkDevice device) noexcept
+{
+    VkShaderModuleCreateInfo info {};
+    info.sType = VK_STRUCTURE_TYPE_SHADER_MODULE_CREATE_INFO;
+    info.codeSize = code.size();
+    info.pCode = reinterpret_cast<const u32*>(code.data());
+
+    VkShaderModule shader_module {};
+    if (vkCreateShaderModule(device, &info, nullptr, &shader_module) != VK_SUCCESS)
+        Logger::fatal_error("Failed to create shader module");
+
+    return shader_module;
+}
+
+void Pipeline::init_renderpass(const VkFormat swapchain_image_format) noexcept
+{
+    static constexpr unsigned COLOR_ATTACHMENT_COUNT = 1;
+
+    if constexpr (Global::IS_DEBUG_BUILD)
+        assert(device_handle != VK_NULL_HANDLE);
+
+    VkAttachmentDescription color_attachment {};
+
+    // format of the color attachment should match format of the swapchain images
+    color_attachment.format = swapchain_image_format;
+
+    // not doing any multisampling yet, so stick to 1 sample
+    color_attachment.samples = VK_SAMPLE_COUNT_1_BIT;
+
+    // determine what to do with the data in the attachment before rendering and after rendering
+
+    // clear the values to constant at start (this will cause the framebuffer to be black before drawing a new frame)
+    color_attachment.loadOp = VK_ATTACHMENT_LOAD_OP_CLEAR; 
+    color_attachment.storeOp = VK_ATTACHMENT_STORE_OP_STORE; // rendered contents will be stored in memory and can be read later
+
+    // stencil load and store operations can be ignored for now
+    color_attachment.stencilLoadOp = VK_ATTACHMENT_LOAD_OP_DONT_CARE;
+    color_attachment.stencilStoreOp = VK_ATTACHMENT_STORE_OP_DONT_CARE;
+
+    // set the layout of the pixels in memory.
+
+    // 'initialLayout' specifies the layout the attachment image will have before the render pass begins.
+    // Using undefined means that we don't care what previous layout the image was in
+    color_attachment.initialLayout = VK_IMAGE_LAYOUT_UNDEFINED;
+
+    // 'finalLayout' specifies the layout to automatically transition to when the render pass finishes.
+    // We want to present the rendered image to the screen, so we transition to VK_IMAGE_LAYOUT_PRESENT_SRC_KHR
+    color_attachment.finalLayout = VK_IMAGE_LAYOUT_PRESENT_SRC_KHR;
+
+    // TODO: add multiple subpasses instead of 1?
+
+    VkAttachmentReference color_attachment_ref {};
+
+    // specifies which attachment to reference by its index in the attachment descriptions array
+    color_attachment_ref.attachment = 0; 
+
+    // specify the layout of the attachment during a subpass that uses this reference
+    color_attachment_ref.layout = VK_IMAGE_LAYOUT_COLOR_ATTACHMENT_OPTIMAL;
+
+    VkSubpassDescription subpass {};
+    subpass.pipelineBindPoint = VK_PIPELINE_BIND_POINT_GRAPHICS;
+
+    // the index of the attachment in this array is directly referenced from the fragment shader 
+    // with the layout(location = 0) out vec4 outColor directive!
+    subpass.colorAttachmentCount = COLOR_ATTACHMENT_COUNT;
+
+    subpass.pColorAttachments = &color_attachment_ref;
+
+    VkRenderPassCreateInfo renderpass_info {};
+    renderpass_info.sType = VK_STRUCTURE_TYPE_RENDER_PASS_CREATE_INFO;
+    renderpass_info.attachmentCount = COLOR_ATTACHMENT_COUNT;
+    renderpass_info.pAttachments = &color_attachment;
+    renderpass_info.subpassCount = 1;
+    renderpass_info.pSubpasses = &subpass;
+
+    if (vkCreateRenderPass(device_handle, &renderpass_info, nullptr, &renderpass) != VK_SUCCESS)
+        Logger::fatal_error("Failed to initialize renderpass");
+}
