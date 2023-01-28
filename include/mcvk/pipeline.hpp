05a6e8ced823ba46d9064fb375de1eb89351bfeb
ramenu <ramenu101@gmail.com>
2023-01-28 16:29:57 -0500

    refactor: add getters to pipeline

--- include/mcvk/pipeline.hpp
+++ include/mcvk/pipeline.hpp
@@ -7,6 +7,8 @@
 #include "logger.hpp"            // for info
 #include "shader.hpp"            // for Shader
 
+namespace Device { class LogicalDevice; }
+
 class Pipeline
 {
     private:
@@ -17,7 +19,7 @@ class Pipeline
         const VkDevice device_handle {VK_NULL_HANDLE};
         void init_renderpass(VkFormat swapchain_image_format) noexcept;
     public:
-        explicit Pipeline(VkDevice device_handle, VkFormat swapchain_image_format) noexcept;
+        Pipeline(const Device::LogicalDevice &device, VkFormat swapchain_image_format) noexcept;
         DELETE_NON_COPYABLE_NON_MOVABLE_DEFAULT(Pipeline)
         ~Pipeline() noexcept
         {
@@ -34,6 +36,8 @@ class Pipeline
             vkDestroyPipelineLayout(device_handle, pipeline_layout, nullptr);
             vkDestroyRenderPass(device_handle, renderpass, nullptr);
         }
+        constexpr const auto &get_renderpass() const noexcept { return renderpass; }
+        constexpr const auto &get_pipeline() const noexcept { return pipeline; }
 
 };
 
